5d82f32aa66276b6ab030f8e506547e0e24e496c
Rijikun <108380085+vvxxzzyy@users.noreply.github.com>
2024-01-03 10:21:19 +0700

    Add files via upload

---
+++ C. Burung Beo.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+   
+   char buff[1001];
+
+int main() {
+   scanf("%[^\n]\n", buff);
+  
+  string s = buff;
+
+  printf("%s\n", s.c_str());
+
+  return 0;
+}
