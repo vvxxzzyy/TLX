dee7e01c03c79ea35fd2e34d3a4fdc42fd1b1e6d
Rijikun <108380085+vvxxzzyy@users.noreply.github.com>
2024-01-06 13:42:20 +0700

    Add files via upload

---
+++ E. Luas Segitiga.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int a, t;
+   scanf("%d %d", &a, &t);
+
+   double luas = ((0.5) * a)*t;
+
+   printf("%.2lf\n", luas);
+  return 0;
+}
