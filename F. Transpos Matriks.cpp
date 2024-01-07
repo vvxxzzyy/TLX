87d97e95562b20902b1ee1bb8e03f8e24cadaacf
Rijikun <108380085+vvxxzzyy@users.noreply.github.com>
2024-01-07 18:33:57 +0700

    Add files via upload

---
+++ F. Transpos Matriks.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int a, b, c;
+   int d, e, f;
+   int g, h, i;
+
+   scanf("%d %d %d\n", &a, &b, &c);
+   scanf("%d %d %d\n", &d, &e, &f);
+   scanf("%d %d %d\n", &g, &h, &i);
+
+   printf("%d %d %d\n", a, d, g);
+   printf("%d %d %d\n", b, e, h);
+   printf("%d %d %d\n", c, f, i);
+
+  return 0;
+}
