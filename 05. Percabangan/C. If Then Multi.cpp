cbed6f0969c7664b321a98d5c2bbcbd4cfee6daf
Rijikun <108380085+vvxxzzyy@users.noreply.github.com>
2024-01-09 06:09:08 +0700

    Add files via upload

---
+++ 05. Percabangan/C. If Then Multi.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int n;
+   scanf("%d\n", &n);
+
+   if (n > 0) {
+      if (n % 2 == 0) { 
+         printf("%d\n", n);
+      }
+   }
+
+  return 0;
+}
