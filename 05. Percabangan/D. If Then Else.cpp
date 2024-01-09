df1144ac6a29f2628fede6c9fca28f3025c9909b
Rijikun <108380085+vvxxzzyy@users.noreply.github.com>
2024-01-10 00:06:19 +0700

    Add files via upload

---
+++ 05. Percabangan/D. If Then Else.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int n;
+   scanf("%d", &n);
+
+   if (n>0) {
+      printf("positif");
+   } else if (n<0) {
+      printf("negatif");
+   } else {
+      printf("nol");
+   }
+
+  return 0;
+}
