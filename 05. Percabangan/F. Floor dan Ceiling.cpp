42d1a13fed6ccc013e4bdb51b341529ab3f4721a
Rijikun <108380085+vvxxzzyy@users.noreply.github.com>
2024-01-12 00:40:24 +0700

    Add files via upload

---
+++ 05. Percabangan/F. Floor dan Ceiling.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   double n;
+   scanf("%lf", &n);
+
+   int f, c, x;
+   x = trunc(n);
+
+   if (n == trunc(n)) {
+      f = x;
+      c = f;
+   } else if (n > 0) {
+      f = x;
+      c = x+1;
+   } else if (n < 0) {
+      f = x-1;
+      c = x;
+   }
+
+   cout << f << " " << c;
+
+  return 0;
+}
