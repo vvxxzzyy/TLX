a7c49ad2a4934d9e5100f60b0da2e0add2668892
Rijikun <108380085+vvxxzzyy@users.noreply.github.com>
2024-01-14 05:51:49 +0700

    Add files via upload

---
+++ 06. Perulangan/D. While + Pencacah.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int n;
+
+   int sum = 0;
+   while (scanf("%d", &n) != EOF) {
+      sum+=n;
+   }
+   
+   printf("%d\n", sum);
+
+   return 0;
+}
